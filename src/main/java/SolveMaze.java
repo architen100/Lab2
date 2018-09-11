53c1d23521f05e127a5c1e550fbd3717a6dbde75
archisha.m <architen100@gmail.com>
2018-09-11 14:02:15 -0500

    Created algorithm to solve the maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,14 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                while (!maze.canMove()) {
+                    maze.turnRight();
+                }
+                maze.canMove();
+            }
+
         }
 
         if (maze.isFinished()) {
